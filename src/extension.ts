cb1761a264c2fceab4cf87dea4bbfb619657010f
kieferrm <kieferrm@gmail.com>
2016-05-21 00:28:36 -0700

    ensure correct media path + refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-import { TextDocumentContentProvider, EventEmitter, Event, Uri, TextDocument, ViewColumn } from "vscode";
+import { ExtensionContext, TextDocumentContentProvider, EventEmitter, Event, Uri, TextDocument, ViewColumn } from "vscode";
 
 const hljs = require('highlight.js');
 const mdnh = require('markdown-it-named-headers');
@@ -19,8 +19,8 @@ const md = require('markdown-it')({
     }
 }).use(mdnh, {});
 
-export function activate(context: vscode.ExtensionContext) {
-    let provider = new MDDocumentContentProvider();
+export function activate(context: ExtensionContext) {
+    let provider = new MDDocumentContentProvider(context);
     let registration = vscode.workspace.registerTextDocumentContentProvider('markdown', provider);
 
     let d1 = vscode.commands.registerCommand('extension.previewMarkdown', () => openPreview());
@@ -34,21 +34,32 @@ export function activate(context: vscode.ExtensionContext) {
           provider.update(markdownPreviewUri);
        }
     });
-    
+
     vscode.workspace.onDidChangeConfiguration(() => {
         vscode.workspace.textDocuments.forEach((document) => {
             if ("markdown" === document.uri.scheme) {
                 provider.update(document.uri);
-            } 
+            }
         });
     });
 }
 
+function endsWith(haystack: string, needle: string): boolean {
+	let diff = haystack.length - needle.length;
+	if (diff > 0) {
+		return haystack.lastIndexOf(needle) === diff;
+	} else if (diff === 0) {
+		return haystack === needle;
+	} else {
+		return false;
+	}
+}
+
 function isMarkdownFile(fileName: string) {
-    return fileName && (fileName.endsWith('.md') 
-          || fileName.endsWith('.mdown')
-          || fileName.endsWith('.markdown')
-          || fileName.endsWith('.markdn'));
+    return fileName && (endsWith(fileName,'.md')
+          || endsWith(fileName, '.mdown')
+          || endsWith(fileName, '.markdown')
+          || endsWith(fileName, '.markdn'));
 }
 
 function openPreview(sideBySide?: boolean): void {
@@ -82,54 +93,60 @@ function getViewColumn(sideBySide): ViewColumn {
     return active.viewColumn;
 }
 
-function fixHref(resource: Uri, href: string) {
-    if (href) {
-
-        // Return early if href is already a URL
-        if (Uri.parse(href).scheme) {
-            return href;
-        }
-
-        // Otherwise convert to a file URI by joining the href with the resource location
-        return Uri.file(path.join(path.dirname(resource.fsPath), href)).toString();
-    }
-
-    return href;
-}
 
 class MDDocumentContentProvider implements TextDocumentContentProvider {
+    private context;
     private _onDidChange = new EventEmitter<Uri>();
 
-    public provideTextDocumentContent(uri: Uri): Thenable<string> {
+    constructor(context: ExtensionContext) {
+        this.context = context;
+    }
 
-        function computeCustomStyleSheetIncludes() : string[] {
-            const styles = vscode.workspace.getConfiguration("markdown")['styles'];
-            if (styles && Array.isArray(styles)) {
-                return styles.map((style) => {
-                    return `<link rel="stylesheet" href="${fixHref(uri, style)}" type="text/css" media="screen">`;
-                });
+    private getMediaPath(mediaFile) {
+        return this.context.asAbsolutePath(path.join('media', mediaFile));
+    }
+
+    private fixHref(resource: Uri, href: string) {
+        if (href) {
+            // Return early if href is already a URL
+            if (Uri.parse(href).scheme) {
+                return href;
             }
-            return [];
+            // Otherwise convert to a file URI by joining the href with the resource location
+            return Uri.file(path.join(path.dirname(resource.fsPath), href)).toString();
         }
+        return href;
+    }
 
+    private computeCustomStyleSheetIncludes(uri) : string[] {
+        const styles = vscode.workspace.getConfiguration("markdown")['styles'];
+        if (styles && Array.isArray(styles)) {
+            return styles.map((style) => {
+                return `<link rel="stylesheet" href="${this.fixHref(uri, style)}" type="text/css" media="screen">`;
+            });
+        }
+        return [];
+    }
+
+    public provideTextDocumentContent(uri: Uri): Thenable<string> {
         return new Promise((approve, reject) => {
             fs.readFile(uri.fsPath, (error, buffer) => {
                 if (error) {
                     return reject(error);
                 }
-                
+
                 const head = [].concat(
                     '<!DOCTYPE html>',
                     '<html>',
                     '<head>',
                     '<meta http-equiv="Content-type" content="text/html;charset=UTF-8">',
-                    `<link rel="stylesheet" type="text/css" href="${path.join(__dirname, '..', '..', 'media', 'markdown.css')}" >`,
-                    `<link rel="stylesheet" type="text/css" href="${path.join(__dirname, '..', '..', 'media', 'tomorrow.css')}" >`,
-                    computeCustomStyleSheetIncludes(),
+                    `<link rel="stylesheet" type="text/css" href="${this.getMediaPath('markdown.css')}" >`,
+                    `<link rel="stylesheet" type="text/css" href="${this.getMediaPath('tomorrow.css')}" >`,
+                    this.computeCustomStyleSheetIncludes(uri),
                     '</head>',
                     '<body class="vs-theme-aware">'
                 ).join('\n');
-                                    
+
                 const body = md.render(buffer.toString());
 
                 const tail = [
@@ -143,7 +160,7 @@ class MDDocumentContentProvider implements TextDocumentContentProvider {
     }
 
     get onDidChange(): Event<Uri> {
-        return this._onDidChange.event; 
+        return this._onDidChange.event;
     }
 
     public update(uri: Uri) {
